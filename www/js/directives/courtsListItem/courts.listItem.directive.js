c1f16329002cbf661d5b5f99c52d12f82ec76256
RJ Pablo <numerianop@sedata.com>
2017-03-12 07:25:02 +0800

    Converted court list item layout to directive Added control panel in Find Courts tab

---
+++ www/js/directives/courtsListItem/courts.listItem.directive.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="courts.listItem.template.html" />
+(function () {
+	'use strict';
+
+	angular.module('mainApp').directive('courtsListItem', courtListItemDirective);
+
+	function courtListItemDirective() {
+		return {
+			//priority: 2,
+			restrict: "EA",
+			templateUrl: '/www/js/directives/courtsListItem/courts.listItem.template.html',
+			link: function (scope, element, attrs, ctrl) {
+				var s = scope;
+			}
+		}
+	}
+})()
