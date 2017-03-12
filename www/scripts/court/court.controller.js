fe0434596414f0f9689be03b8e4686d09a5e1460
RJ Pablo <numerianop@sedata.com>
2017-03-12 18:05:04 +0800

    Set up modal for court registration

--- www/scripts/court/court.controller.js
+++ www/scripts/court/court.controller.js
@@ -3,62 +3,109 @@
 
     angular
         .module('mainApp')
-        .controller('courtCtrl', ['$scope', courtCtrl]);
+        .controller('courtCtrl', ['$scope','$uibModal', '$document', courtCtrl])
+        .controller('addCourtCtrl', ['$uibModalInstance', addCourtCtrl]);
 
-    function courtCtrl() {
+    function courtCtrl($scope, $uibModal, $document) {
+
+        var __this = this;
 
         this.courtsSearched = [
             {
+                id: 1,
                 name: 'Ballers Court',
                 location: '#123 Paraiso St., Makati City, Manila',
-                rate: '200 per hour',
+                rate: '200',
                 contactNo: '+63 932 987 7865',
                 imagePath: 'images/sample images/courts/1.jpg'
             },
             {
+                id: 2,
                 name: 'Masters Court',
                 location: '#123 Pinaglabanan St., San Juan, Manila',
-                rate: '250 per hour',
+                rate: '250',
                 contactNo: '+63 932 123 7865',
                 imagePath: 'images/sample images/courts/2.jpg'
             },
             {
+                id: 3,
                 name: 'XYZ Basketball Court',
                 location: '#33 Connecticut St., Sampaloc City, Manila',
-                rate: '500 per hour',
+                rate: '500',
                 contactNo: '+63 932 123 5665',
                 imagePath: 'images/sample images/courts/3.jpg'
             },
             {
+                id: 4,
                 name: 'Ballers Court',
                 location: '#123 Paraiso St., Makati City, Manila',
-                rate: '200 per hour',
+                rate: '200',
                 contactNo: '+63 932 987 7865',
-                imagePath: 'images/sample images/courts/1.jpg'
+                imagePath: 'images/sample images/courts/4.jpg'
             },
             {
+                id: 5,
                 name: 'Masters Court',
                 location: '#123 Pinaglabanan St., San Juan, Manila',
-                rate: '250 per hour',
+                rate: '250',
                 contactNo: '+63 932 123 7865',
-                imagePath: 'images/sample images/courts/2.jpg'
+                imagePath: 'images/sample images/courts/5.jpg'
             },
             {
+                id: 6,
                 name: 'XYZ Basketball Court',
                 location: '#33 Connecticut St., Sampaloc City, Manila',
-                rate: '500 per hour',
+                rate: '500',
                 contactNo: '+63 932 123 5665',
-                imagePath: 'images/sample images/courts/3.jpg'
+                imagePath: 'images/sample images/courts/2.jpg'
             }
-        ]
+        ];//Sample Courts
 
-        this.tabs = [
-            { heading: 'Dynamic Title 1', content: 'Dynamic content 1' },
-            { heading: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
-        ];
+        this.animationsEnabled = true;
 
-        this.model = {
-            name: 'Tabs'
+        this.openAddModal = function (size, parentSelector) {
+            var parentElem = parentSelector ?
+              angular.element($document[0].querySelector(parentSelector)) : undefined;
+            var addCourtModal = $uibModal.open({
+                animation: this.animationsEnabled,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: '/www/views/court/add-modal-template.html',
+                controller: 'addCourtCtrl',
+                controllerAs: 'addCourtCtrl',
+                size: size,
+                appendTo: parentElem
+                //resolve: {
+                //    items: function () {
+                //        return this.items;
+                //    }
+                //}
+            });
+
+            addCourtModal.result.then(function (court) {
+                __this.addCourt(court);
+            }, function (reason) {
+                console.log('Reason for closing: ' + reason);
+            });
+        };
+
+        this.addCourt = function (court) {
+            this.courtsSearched.push(court);
+        }
+
+    };
+
+    function addCourtCtrl($uibModalInstance) {
+
+        this.court = { imagePath: 'images/sample images/courts/1.jpg' }
+
+
+        this.ok = function () {
+            $uibModalInstance.close(this.court);
+        };
+
+        this.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
         };
     }
 
