8aae8062d818a899321a9522bf1913d55dc3a8bc
RJ Pablo <numerianop@sedata.com>
2017-05-27 05:50:22 +0800

    Added rating directive

---
+++ www/js/directives/rating/rating.directive.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="courts.listItem.template.html" />
+(function () {
+	'use strict';
+
+	angular.module('mainApp').directive('rating', courtListItemDirective);
+
+	function courtListItemDirective() {
+		return {
+		    restrict: 'AE',
+		    templateUrl: '/js/directives/rating/rating.directive.template.html',
+		    scope:{
+                value: '=value'
+		    }
+		}
+	}
+})()
