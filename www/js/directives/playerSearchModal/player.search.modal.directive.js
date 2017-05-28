d17cd6f034c98b33d683bf12dbf239cac4a613d3
RJ Pablo <numerianop@sedata.com>
2017-05-29 00:34:00 +0800

    Added playerSearchModal directive Modified rating directive to accept values from 0-5 only [All Players module] Added criteria to search function

---
+++ www/js/directives/playerSearchModal/player.search.modal.directive.js
@@ -0,0 +1,55 @@
+﻿/// https://jsfiddle.net/ValentinH/954eve2L/ for examples on rz-slider />
+(function () {
+    'use strict';
+
+    angular.module('mainApp').directive('playerSearchModal', ['$uibModal', directiveFn]);
+
+    function directiveFn($uibModal) {
+        return {
+            restrict: "A",
+            scope: {
+                searchCriteria: '=?',
+                onSubmit: '&'
+            },
+            link: function (scope, element, attrs) {
+
+                element.on('click', showDialog);
+
+                function showDialog() {
+                    var courtSearchModal = $uibModal.open({
+                        animation: true,
+                        backdrop: 'static',
+                        windowClass: 'player-search-modal',
+                        ariaLabelledBy: 'modal-title',
+                        ariaDescribedBy: 'modal-body',
+                        templateUrl: '/js/directives/playerSearchModal/player.search.modal.template.html',
+                        resolve: {
+                            searchCriteria: scope.searchCriteria
+                        },
+                        controller: function ($scope, commonServices, $uibModalInstance, $timeout, searchCriteria) {
+                            
+                            $scope.searchCriteria = angular.copy(searchCriteria);
+
+                            $scope.ok = function (e) {
+                                searchCriteria = angular.copy($scope.searchCriteria)
+                                $uibModalInstance.close($scope.searchCriteria);
+                            };
+
+                            $scope.cancel = function (e) {
+                                $uibModalInstance.dismiss('cancel');
+                            };
+
+                        },
+                        size: 'md'
+                    });
+                    courtSearchModal.result.then(function (searchCriteria) {
+                        scope.searchCriteria = searchCriteria;
+                        scope.onSubmit({ criteria: searchCriteria })
+                    }, function (reason) {
+                        //commonServices.toast.info('No new court was added.')
+                    });
+                }
+            }
+        }
+    }
+})()
--- www/views/players/all/all.players.controller.js
+++ www/views/players/all/all.players.controller.js
@@ -20,6 +20,7 @@
             winRateMax: 100
         }
 
+        vm.searchCriteriaStr = ''
         vm.sortReverse = true;
         vm.sortIndex = 0;
         vm.sortObjects = [
@@ -98,16 +99,127 @@
         }
 
         vm.search = function (criteria) {
-            courtContext.getCourts(criteria).then(
+            vm.searchCriteria = criteria;
+            buildSearchCriteriaStr()
+            playersContext.searchPlayers(criteria).then(
                 function (res) {
-
-                    vm.filtered = true;
-
+                    vm.searchCriteria = criteria;
+                    vm.filtered = vm.searchCriteriaStr.trim() != '';
+                    vm.players = res.data;
                 }, function (err) {
                     commonServices.handleError(err)
                 }
             )
+        }
 
+        function buildSearchCriteriaStr() {
+            var str = '';
+
+            if (vm.searchCriteria.playerName) {
+                str = str + 'Name contains \'' + vm.searchCriteria.playerName + '\', '
+            }
+            if (vm.searchCriteria.sex) {
+                str = str + (vm.searchCriteria.sex=='M'?'Male':'Female') + ', '
+            }
+            if (vm.searchCriteria.heightFtMin && vm.searchCriteria.heightInMin) {
+                str = str + 'Min. Height: ' + vm.searchCriteria.heightFtMin + '\' ' + vm.searchCriteria.heightInMin + '", '
+            }
+            if (vm.searchCriteria.heightFtMax && vm.searchCriteria.heightInMax) {
+                str = str + 'Max. Height: ' + vm.searchCriteria.heightFtMax + '\' ' + vm.searchCriteria.heightInMax + '", '
+            }
+            if (vm.searchCriteria.mvpsMin) {
+                if (vm.searchCriteria.mvpsMax) {
+                    if (vm.searchCriteria.mvpsMin == vm.searchCriteria.mvpsMax) {
+                        str = str + 'MVPs: ' + vm.searchCriteria.mvpsMin + ', '
+                    } else {
+                        str = str + 'MVPs: ' + vm.searchCriteria.mvpsMin + ' - ' + vm.searchCriteria.mvpsMax + ', '
+                    }
+                } else {
+                    str = str + 'MVPs: at least ' + vm.searchCriteria.mvpsMin + ', '
+                }                
+            } else if (vm.searchCriteria.mvpsMax) {
+                str = str + 'MVPs: 0 - ' + vm.searchCriteria.mvpsMax + ', '
+            }
+            if (vm.searchCriteria.dribblingMin) {
+                if (vm.searchCriteria.dribblingMax) {
+                    if (vm.searchCriteria.dribblingMin == vm.searchCriteria.dribblingMax) {
+                        str = str + 'Dribbling Skills: ' + vm.searchCriteria.dribblingMin + ' star(s), '
+                    } else {
+                        str = str + 'Dribbling Skills: ' + vm.searchCriteria.dribblingMin + ' - ' + vm.searchCriteria.dribblingMax + ' star(s), '
+                    }
+                } else {
+                    str = str + 'Dribbling Skills: at least ' + vm.searchCriteria.dribblingMin + ' star(s), '
+                }
+            } else if (vm.searchCriteria.dribblingMax) {
+                str = str + 'Dribbling Skills: 0 - ' + vm.searchCriteria.dribblingMax + ' star(s), '
+            }
+            if (vm.searchCriteria.shootingMin) {
+                if (vm.searchCriteria.shootingMax) {
+                    if (vm.searchCriteria.shootingMin == vm.searchCriteria.shootingMax) {
+                        str = str + 'Shooting Skills: ' + vm.searchCriteria.shootingMin + ' star(s), '
+                    } else {
+                        str = str + 'Shooting Skills: ' + vm.searchCriteria.shootingMin + ' - ' + vm.searchCriteria.shootingMax + ' star(s), '
+                    }
+                } else {
+                    str = str + 'Shooting Skills: at least ' + vm.searchCriteria.shootingMin + ' star(s), '
+                }
+            } else if (vm.searchCriteria.shootingMax) {
+                str = str + 'Shooting Skills: 0 - ' + vm.searchCriteria.shootingMax + ' star(s), '
+            }
+            if (vm.searchCriteria.threePtsMin) {
+                if (vm.searchCriteria.threePtsMax) {
+                    if (vm.searchCriteria.threePtsMin == vm.searchCriteria.threePtsMax) {
+                        str = str + '3-Point Skills: ' + vm.searchCriteria.threePtsMin + ' star(s), '
+                    } else {
+                        str = str + '3-Point Skills: ' + vm.searchCriteria.threePtsMin + ' - ' + vm.searchCriteria.threePtsMax + ' star(s), '
+                    }
+                } else {
+                    str = str + '3-Point Skills: at least ' + vm.searchCriteria.threePtsMin + ' star(s), '
+                }
+            } else if (vm.searchCriteria.threePtsMax) {
+                str = str + '3-Point Skills: 0 - ' + vm.searchCriteria.threePtsMax + ' star(s), '
+            }
+            if (vm.searchCriteria.shootingMin) {
+                if (vm.searchCriteria.shootingMax) {
+                    if (vm.searchCriteria.shootingMin == vm.searchCriteria.shootingMax) {
+                        str = str + 'Shooting Skills: ' + vm.searchCriteria.shootingMin + ' star(s), '
+                    } else {
+                        str = str + 'Shooting Skills: ' + vm.searchCriteria.shootingMin + ' - ' + vm.searchCriteria.shootingMax + ' star(s), '
+                    }
+                } else {
+                    str = str + 'Shooting Skills: at least ' + vm.searchCriteria.shootingMin + ' star(s), '
+                }
+            } else if (vm.searchCriteria.shootingMax) {
+                str = str + 'Shooting Skills: 0 - ' + vm.searchCriteria.shootingMax + ' star(s), '
+            }
+            if (vm.searchCriteria.passingMin) {
+                if (vm.searchCriteria.passingMax) {
+                    if (vm.searchCriteria.passingMin == vm.searchCriteria.passingMax) {
+                        str = str + 'Passing Skills: ' + vm.searchCriteria.passingMin + ' star(s), '
+                    } else {
+                        str = str + 'Passing Skills: ' + vm.searchCriteria.passingMin + ' - ' + vm.searchCriteria.passingMax + ' star(s), '
+                    }
+                } else {
+                    str = str + 'Passing Skills: at least ' + vm.searchCriteria.passingMin + ' star(s), '
+                }
+            } else if (vm.searchCriteria.passingMax) {
+                str = str + 'Passing Skills: 0 - ' + vm.searchCriteria.passingMax + ' star(s), '
+            }
+            if (vm.searchCriteria.defenceMin) {
+                if (vm.searchCriteria.defenceMax) {
+                    if (vm.searchCriteria.defenceMin == vm.searchCriteria.defenceMax) {
+                        str = str + 'Defensive Skills: ' + vm.searchCriteria.defenceMin + ' star(s), '
+                    } else {
+                        str = str + 'Defensive Skills: ' + vm.searchCriteria.defenceMin + ' - ' + vm.searchCriteria.defenceMax + ' star(s), '
+                    }
+                } else {
+                    str = str + 'Defensive Skills: at least ' + vm.searchCriteria.defenceMin + ' star(s), '
+                }
+            } else if (vm.searchCriteria.defenceMax) {
+                str = str + 'Defensive Skills: 0 - ' + vm.searchCriteria.defenceMax + ' star(s), '
+            }
+
+            vm.searchCriteriaStr = str
         }
 
         this.resetFilters = function () {
