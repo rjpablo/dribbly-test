c3fe06b3daad98796331db3a5519d0aec4f868a0
RJ Pablo <numerianop@sedata.com>
2017-06-12 22:51:01 +0800

    Added teamslistItem directive

---
+++ www/js/directives/teamsListItem/teams.listItem.directive.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="courts.listItem.template.html" />
+(function () {
+	'use strict';
+
+	angular.module('mainApp').directive('teamsListItem', ['settings',courtListItemDirective]);
+
+	function courtListItemDirective(settings) {
+		return {
+			//priority: 2,
+			restrict: "E",
+			scope: {
+			    'team': '=',
+			},
+            replace: true,
+            templateUrl: '/js/directives/teamsListItem/teams.listItem.template.html',
+            link: function (scope, element, attrs) {
+                scope.$settings = settings
+            }
+		}
+	}
+})()
