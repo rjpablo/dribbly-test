1b2a1a64ac79c7ca916484e00c22d8f2d277d063
RJ Pablo <numerianop@sedata.com>
2017-06-12 19:59:58 +0800

    Added games-list-item directive Added display of player games in player profile screen

---
+++ www/js/directives/gamesListItem/games.listItem.directive.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="courts.listItem.template.html" />
+(function () {
+	'use strict';
+
+	angular.module('mainApp').directive('gamesListItem', ['settings',courtListItemDirective]);
+
+	function courtListItemDirective(settings) {
+		return {
+			//priority: 2,
+			restrict: "E",
+			scope: {
+			    'game': '=',
+			    'iconUrlPrefix': '='
+			},
+            replace: true,
+            templateUrl: '/js/directives/gamesListItem/games.listItem.template.html',
+            link: function (scope, element, attrs) {
+                scope.$settings = settings
+            }
+		}
+	}
+})()
