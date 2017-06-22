0b10b653e54e2210769a082acd39d267205ea16b
RJ Pablo <numerianop@sedata.com>
2017-06-22 13:35:51 +0800

    [index.html]Added comments needed by gulp-useref [gulpfile.js] Added tasks for building app

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 /// <binding AfterBuild='watch' />
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    useref = require('gulp-useref');
 
-gulp.task('watch',['browserSync'], function () {
-    gulp.watch('www/**/*.css', browserSync.reload);
-    gulp.watch('www/**/*.html', browserSync.reload);
-    gulp.watch('www/**/*.js', browserSync.reload);
+gulp.task('html', function () {
+    return gulp.src(['./www/**/!(index)*.html'])
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('www/**/*.css', ['js-css'], browserSync.reload);
+    gulp.watch('www/**/*.html', ['html'], browserSync.reload);
+    gulp.watch('www/**/*.js', ['js-css'], browserSync.reload);
     // Other watchers
 })
 
@@ -15,11 +24,44 @@ gulp.task('browserSync', function () {
     browserSync.init({
         startPath: '/',
         server: {
+            baseDir: 'dist'
+        },
+        port: 8888
+    })
+})
+
+gulp.task('browserSync-dev', function () {
+    browserSync.init({
+        server: {
             baseDir: 'www',
             routes: {
                 '/bower_components': 'bower_components'
             }
         },
-        port:8888
+        port: 8888
     })
 })
+
+gulp.task('js-css', function () {
+    return gulp.src('www/*.html')
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+    return gulp.src(['www/images/**/*.ico', 'www/images/**/*.png', 'www/images/**/*.jpg'])
+        .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('fonts', function () {
+    return gulp.src(['www/fonts/**/*'])
+        .pipe(gulp.dest('dist/fonts/'));
+});
+
+gulp.task('build', function () {
+    gulp.start(['html','js-css','fonts','images', 'browserSync', 'watch'])
+});
+
+gulp.task('build-dev', function () {
+    gulp.start(['browserSync-dev', 'watch'])
+});
