232eb78148151276dc04698f25f79d9551d3c14c
RJ Pablo <numerianop@sedata.com>
2017-06-28 15:58:38 +0800

    Added uriEncode filter

---
+++ www/scripts/filters/uriEncode_filter.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('mainApp')
+    .filter('uriEncode', function () {
+        return function (input) {
+            if (input) {
+                return window.encodeURIComponent(input);
+            }
+            return "";
+        }
+    });
+
+})();
