fda4072412b6e6af8b3487f326ec6e6f91dc05ad
RJ Pablo <numerianop@sedata.com>
2017-06-30 18:37:39 +0800

    Added team-member-directive

---
+++ www/scripts/directives/teamMember/team-member-directive.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('mainApp').directive('teamMember', ['settings',directiveFn]);
+
+    function directiveFn(settings) {
+        return {
+            restrict: 'E',
+            scope: {
+                'player': '=',
+                'removeFunction': '&?'
+            },
+            replace: true,
+            templateUrl: '/scripts/directives/teamMember/team-member-directive-template.html',
+            link: function (scope, element, attrs) {
+                
+                function setPicUrl(player) {
+                    if (scope.player) {
+                        if (player.profilePic) {
+                            scope.profilePicUrl = settings.fileUploadBasePath + player.playerId + '/photos/' + player.profilePic
+                        } else {
+                            scope.profilePicUrl = settings.defaultProfilePicUrl;
+                        }
+                    }
+                }
+
+                scope.$watch('player', function (player) {
+                    setPicUrl(player);
+                })
+            }
+        }
+    }
+})();
