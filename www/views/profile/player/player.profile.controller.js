1c0d405ce9fcf01266e4c35534202145128b47f8
RJ Pablo <numerianop@sedata.com>
2017-10-13 17:44:57 +0800

    Added filter for player teams

--- www/views/profile/player/player.profile.controller.js
+++ www/views/profile/player/player.profile.controller.js
@@ -4,17 +4,22 @@
     angular
         .module('mainApp')
         .controller('playerProfileController', ['$scope', 'settings', 'httpService', 'commonServices',
-            'authentication', '$location', '$state', 'profileContext', 'playersContext', controllerFn]);
+            'authentication', '$location', '$state', 'profileContext', 'playersContext', 'filterFilter', controllerFn]);
 
     function controllerFn($scope, settings, httpService, commonServices, authentication,
-        $location, $state, profileContext, playersContext) {
+        $location, $state, profileContext, playersContext, filterFilter) {
 
         var vm = this;
+        vm.teamFilter;
+        vm.filteredTeams = [];
+        vm.currentTeamFilter = { isCurrentMember: true };
+        vm.previousTeamFilter = { isCurrentMember: false };
 
         profileContext.getPlayerProfile($scope.profile.details.userId).then(
             function (res) {
                 if (res.data) {
-                    $scope.profile.player = res.data
+                    $scope.profile.player = res.data;
+                    vm.filterTeams('current');
                 } else {
 
                 }
@@ -36,6 +41,16 @@
                 }, function (err) {
                     commonServices.handleError(err)
                 })
+        }
+        vm.filterTeams = function (filter) {
+            vm.teamFilter = filter;
+            if (filter == 'current') {
+                vm.filteredTeams = filterFilter($scope.profile.player.teams, vm.currentTeamFilter);
+            } else if (filter == 'previous') {
+                vm.filteredTeams = filterFilter($scope.profile.player.teams, vm.previousTeamFilter);
+            } else {
+                vm.filteredTeams = $scope.profile.player.teams
+            }
         }
 
     };
